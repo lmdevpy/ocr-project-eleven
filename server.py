18fa18f5f8ae00088ecde85738a8ee0b66b6b28b
lmdevpy <mous0782@gmail.com>
2023-12-11 22:57:08 +0100

    error 1 fix

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask,render_template,request,redirect,flash,url_for, abort
+
 
 
 def loadClubs():
@@ -26,9 +27,12 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
-
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        flash("Invalid email adress")
+        return render_template('index.html'), 401
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
---
+++ tests/unit_test/test_showsummary.py
@@ -0,0 +1,12 @@
+import pytest
+
+def test_showSummary_with_valid_email(client):
+    response = client.post('/showSummary', data={'email': 'john@simplylift.co'})
+    assert response.status_code == 200
+
+def test_showSummary_with_invalid_email(client):
+    response = client.post('/showSummary', data={'email': 'invalid@example.com'})
+    assert response.status_code == 401
+
+if __name__ == '__main__':
+    pytest.main()
